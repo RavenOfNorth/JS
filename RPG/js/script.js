56a1316b7d026313d027ffc1cf259ef876d5e4b1
RavenOfNorth <dmitrygavvrilenkojob@gmail.com>
2024-04-14 15:08:53 +0300

    update script.js
    update some funcs and add new func to sell weapon if it needed

--- RPG/js/script.js
+++ RPG/js/script.js
@@ -91,14 +91,13 @@ function buyHealth() {
       health += 10;
       goldText.innerText = gold;
       healthText.innerText = health;
-    }
-    else {
+    } else {
         text.innerText = "You do not have enough gold to buy health."
     }
   }
 
 function buyWeapon() {
-    if (currentWeapon < 3) {
+    if (currentWeapon < weapons.length - 1) {
         if (gold >= 30) {
             gold -= 30;
             currentWeapon ++;
@@ -108,9 +107,21 @@ function buyWeapon() {
             inventory.push(newWeapon);
             text.innerText += " In your inventory you have: " + inventory
         } else {
-            text.innerText = "You do not have enough gold to buy a weapon."
+            text.innerText = "You do not have enough gold to buy a weapon.";
         }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
+}
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold
+    }
+
 }
 
 function fightSlime() {
