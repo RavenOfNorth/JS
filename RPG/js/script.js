1c3647fa6c23db71a16256bfc33983eab7df65be
RavenOfNorth <dmitrygavvrilenkojob@gmail.com>
2024-04-15 19:45:41 +0300

    Update script.js
    add Easter Egg :D

--- RPG/js/script.js
+++ RPG/js/script.js
@@ -70,6 +70,12 @@ const locations = [
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
       },
+      {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+      },
   ];
 
 // initialize buttons
@@ -239,5 +245,40 @@ function restart() {
     goldText.innerText = gold;
     healthText.innerText = health;
     xpText.innerText = xp;
-    goTown()
+    goTown();
+}
+
+function easterEgg() {
+    update(locations[7]);
+}
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+        if (numbers.includes(guess)) {
+            text.innerText += "Right! You win 20 gold!";
+            gold += 20;
+            goldText.innerText = gold;
+        } else {
+            text.innerText += "Wrong! You lose 10 health!";
+            health -= 10;
+            healthText.innerText = health;
+        }
+        if (health <= 0) {
+            lose();
+        }
+    }
+}
+
+function pickTwo() {
+    pick(2);
+}
+
+function pickEight() {
+    pick(8);
 }
