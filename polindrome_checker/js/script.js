e388816ec834a933f24d2cf044d5c1a8f3745c67
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-04-28 01:38:10 +0300

    script js is complete
    script is complete

--- polindrome_checker/js/script.js
+++ polindrome_checker/js/script.js
@@ -1,33 +1,17 @@
-const getValue = document.getElementById("text-input");
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
-// TODO: connect that funcs to index.html
-
-function isInvalidInputChecker(str) {
-    if (str.length < 1) {
-        alert("Please input a value");
+// Check is valid input (input shouldn't be empty)
+checkButton.addEventListener("click", () => {
+    const replaced = textInput.value.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    if (textInput.value === "") {
+        alert("Please input a value")
+    } else if (textInput.value.length === 1) {
+        result.innerText = `${textInput.value} is a palindrome`
+    } else if (replaced === [...replaced].reverse().join("")) {
+        result.innerText = `${textInput.value} is a palindrome`;
     } else {
-        return;
+        result.innerText = `${textInput.value} is not a palindrome`;
     }
-}
-
-function isPolindromeChecker(str) {
-
-    // Take str element to lowercase and delete spaces
-    str = str.toLowerCase().replace(/\s/g, '');
-
-    // Reverse str element
-    const reversedStr = str.split("").reverse().join('');
-    return str === reversedStr;
-}
-
-function getResult (value) {
-    if (value === true) {
-        console.log(`${getValue} is a palindrome`);
-    } else {
-        console.log(`${getValue} isn't a palindrome`);
-    }
-}
-
-// console.log(isPolindromeChecker("abbac"));
-
-getResult(isPolindromeChecker("abba"));
+});
