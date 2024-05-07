1c03727ea3b72db888ac0433b272d5bbac0ff146
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-08 02:01:01 +0300

    Update script.js

--- TODO_app/js/script.js
+++ TODO_app/js/script.js
@@ -87,6 +87,10 @@ const reset = () => {
     currentTask = {};
 };
 
+if (taskData.length) {
+  updateTaskContainer();
+}
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
