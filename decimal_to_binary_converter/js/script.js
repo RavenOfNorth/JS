2f87d5f1884981703960e21891cb9190eeb99024
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-09 14:32:17 +0300

    new Decimal to binary func

--- decimal_to_binary_converter/js/script.js
+++ decimal_to_binary_converter/js/script.js
@@ -3,28 +3,16 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 function decimalToBinary(input) {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
+    let binary = "";
+    result.innerText = binary;
 
     if (input === 0) {
-        result.innerText = "0";
-        return
-    };
+        binary = "0";
+    }
 
-    while(input > 0) {
-        const quotient = Math.floor(input / 2);
-        const remainder = input % 2;
-
-        inputs.push(input);
-        remainders.push(remainder);
-        quotients.push(quotient);
-
-        console.log("Inputs: ", inputs);
-        console.log("Quotients: ", quotients);
-        console.log("Remainders: ", remainders);
-
-        result.innerText = remainders.reverse().join("");
+    while (input > 0) {
+        input = Math.floor(input / 2);
+        binary = (input % 2) + binary;
     };
 };
 
