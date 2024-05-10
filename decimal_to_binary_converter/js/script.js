754277193f00f2ccfb8956be5d557866478f4f85
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-11 00:28:22 +0300

    Update script.js

--- decimal_to_binary_converter/js/script.js
+++ decimal_to_binary_converter/js/script.js
@@ -2,29 +2,45 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-function decimalToBinary(input) {
-    if (input === 0 || input === 1) {
-        return String(input);
-    } else {
-        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
-    }
+const decimalToBinary = (input) => {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
 };
 
-//TODO: Create new func for animation
-
-function checkUserInput() {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
-        window.alert("Please provide a decimal number");
-        return
-    }
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = '';
-};
-
-// add event's leasteners here:
-convertBtn.addEventListener('click', checkUserInput);
-numberInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        checkUserInput();
+const showAnimation = () => {
+    setTimeout(() => {
+        console.log("free")
+    }, 500);
+    setTimeout(() => {
+        console.log("Code");
+      }, 1000);
+      console.log("Camp");
     };
-});
+
+const checkUserInput = () => {
+    const inputInt = parseInt(numberInput.value);
+  
+    if (!numberInput.value || isNaN(inputInt)) {
+      alert("Please provide a decimal number");
+      return;
+    }
+  
+    if (inputInt === 5) {
+      showAnimation();
+      return;
+    }
+  
+    result.textContent = decimalToBinary(inputInt);
+    numberInput.value = "";
+  };
+  
+  convertBtn.addEventListener("click", checkUserInput);
+  
+  numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkUserInput();
+    }
+  });
