f0dc8ce86a5c662f8a2ca07030d2a3a0c9750669
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-11 00:09:04 +0300

    Update script.js

--- decimal_to_binary_converter/js/script.js
+++ decimal_to_binary_converter/js/script.js
@@ -2,17 +2,15 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-//TODO Continue to update decimalToBinary func with recursion
-
 function decimalToBinary(input) {
-    if (input === 0) {
-        return "0";
-    } else if (input === 1) {
-        return "1";
+    if (input === 0 || input === 1) {
+        return String(input);
     } else {
         return decimalToBinary(Math.floor(input / 2)) + (input % 2);
     }
 };
+
+//TODO: Create new func for animation
 
 function checkUserInput() {
     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
