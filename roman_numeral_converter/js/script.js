ce312329b2610114cae0e97ae8b897636d2613da
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-12 13:09:37 +0300

    Update script.js

--- roman_numeral_converter/js/script.js
+++ roman_numeral_converter/js/script.js
@@ -18,3 +18,32 @@ const isValidInput = () => {
         }
     }
 };
+
+const convertDecimalToRoman = (num) => {
+    const romanNums = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    let convert_result = "";
+
+    for (let key in romanNums) {
+        while (num >= romanNums[key]) {
+            convert_result += key;
+            num -= romanNums[key];
+        }
+    }
+
+    return convert_result;
+}
