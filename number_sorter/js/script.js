2bb9a63eec86b6fb31c2e2afc2d153ed04ee89cf
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-14 12:14:02 +0300

    Update script.js

--- number_sorter/js/script.js
+++ number_sorter/js/script.js
@@ -3,8 +3,30 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
 
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown => dropdown.value);
-    console.log(inputValues); // TODO: DELETE IT LATER!
+    const inputValues = [
+        ...document.getElementsByClassName("values-dropdown")
+    ].map(dropdown => Number(dropdown.value));
+
+    updateUI(inputValues);
+};
+
+sortButton.addEventListener("click", sortInputArray);
+
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+    });
+
+};
+
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        // Why tests think that inner for loop not empty?
+        for (let j = 0; j < array.length - i - 1; j++) {
+
+        };
+    };
 };
 
 sortButton.addEventListener("click", sortInputArray);
